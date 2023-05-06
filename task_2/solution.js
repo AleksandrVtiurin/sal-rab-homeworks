6048502de7d2b55f256aae585146c59c30a4ce77
Change <clifisasha@gmail.com>
2023-05-06 16:47:17 +0600

    changeTask2

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,15 +5,21 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum; // создайте переменную shippingSum
 
-    // если productsSum равно 0,
+    if(productsSum ==0) {
+        shippingSum =0;
+    } // если productsSum равно 0,
     // то shippingSum присвоить значение 0
 
-    // если productsSum Больше или равна freeShippingMinSum,
+    if(productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }// если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
 
-    // если productsSum больше 0 и меньше freeShippingMinSum,
+    if((productsSum > 0) && (productsSum < freeShippingMinSum)) {
+        shippingSum = shippingPrice;
+    } // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
@@ -28,9 +34,13 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    // создайте переменную discountSum
+    let discountSum// создайте переменную discountSum
 
-    // если productsSum больше или равно discountMinSum,
+    if(productsSum >= discountMinSum) {
+        discountSum = discountPart / 100 * discountSum;
+    } else{
+        discountSum = 0;
+    }// если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
